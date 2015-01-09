088492b0eb1e881412546a09b6b538a2851a5ef9
nimblegirl <aerials2nowhere@gmail.com>
2015-01-10 00:19:16 +0200

    Finish user singup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   root 'static_pages#home'
 
   match('/help', {to: 'static_pages#help', via: 'get'})
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+  	name "Valentina"
+  	email "nimblegirl@mail.ru"
+  	password "foobar"
+  	password_confirmation "foobar"
+  end
+end
