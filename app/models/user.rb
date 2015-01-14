59751694b2a7eb560ffc52dc3e6aeb721c77e663
valentina <aerials2nowhere@gmail.com>
2015-01-14 14:57:58 +0200

    Finish user microposts interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,11 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)	  
   end
 
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
+
 private
 	
   def create_remember_token
